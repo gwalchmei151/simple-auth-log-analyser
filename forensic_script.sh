30b9d70cdd815fc066e98a5aa204528148b60145
Tushar <gwalchmei@gmail.com>
2021-08-25 00:08:37 +0800

    includes duplicate count
    Alerts user to the presence of duplicate whole entries, and number of occurrences.
    Total failed attempt count is now the sum of those occurrences with the initial awk loop

--- forensic_script.sh
+++ forensic_script.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-echo "Ensure that you are in the same directory as the target file"
+echo "Ensure that you are in the same directory as the file"
 echo "Enter filename including extension (e.g auth.log)"
 read fn
 
-awk '/Failed/ {++failedList[$0]}
+DUPCOUNT=$(awk '/Failed/ {dups[$0]++} END{for (num in dups) 
+	if (dups[num]>1)		
+		{print "\n Duplicate whole entries found for failed attempts logged below:\n", "\n "dups[num],"entries for ---> ", num}}' $fn | grep -c "entries for ---> ")
+		
+awk '/Failed/ {dups[$0]++} END{for (num in dups) 
+	if (dups[num]>1)		
+		{print "\n WARNING!! Duplicate whole entries found for failed attempts logged below:\n", "\n\t "dups[num],"entries for ---> ", num}}' $fn
+
+FAILCOUNT=$(awk '/Failed/ {++failedList[$0]}
 	
 	END {
 		
 		for (attempts in failedList) {
 			count++
 			}
-		printf("\n Number of Failed attempts: %d\n", count)
-		}' $fn
+		printf("%d", count)
+		}' $fn)
+		
+TOTALFAIL=$(( FAILCOUNT + DUPCOUNT ))
+
+echo
+echo " 1) Number of Failed attempts including duplicates: " "$TOTALFAIL"
 		
 #Following block of code to get IP addresses from attacks is taken from https://gist.github.com/c25d729784ee1c3e88be240ac2177554.git 
 declare -a badstrings=("Failed password for invalid user"
@@ -41,7 +54,7 @@ awk '{!failedIP[$0]++}
 		for (ip in failedIP) {
 			count++
 			}
-		printf("\n Number of IP addresses from failed attempts: %d\n", count)
+		printf("\n 2) Number of IP addresses from failed attempts: %d\n", count)
 		} ' temp.txt
 
 
@@ -50,7 +63,7 @@ rm "temp.txt"
 
 		
 awk '/Failed/ && !/invalid/ {!list[$9]++}
-	BEGIN {print "\n Valid user(s) with failed attempt(s): "}
+	BEGIN {print "\n 3) Valid user(s) with failed attempt(s): "}
 	END {
 		for (name in list) {
 			count++
@@ -64,13 +77,13 @@ awk '/Invalid user/ {invalidUser[$0]++}
 		for (user in invalidUser) {
 			count++
 			}
-		printf(" Invalid user attempts: %d\n", count)
+		printf(" 4) Invalid user attempts: %d\n", count)
 		}' $fn
 		
-awk '/Failed/ {print $(NF-3)}' $fn | sort | uniq -c | sort -n | tail -n 1 | awk '{print "\n IP address", $2, "attacked the most for a total of", $1, "times!"}'
+awk '/Failed/ {print $(NF-3)}' $fn | sort | uniq -c | sort -n | tail -n 1 | awk '{print "\n 5) IP address", $2, "attacked the most for a total of", $1, "times!"}'
 
 ip=$(awk '/Failed/ {print $13}' $fn | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}')
 echo
-echo -n " "$ip " is from "
+echo -n " 6)" " "$ip " is from "
 cc=$(whois $ip | awk 'tolower($1) ~/country/ {print $2}')
 curl -L -s https://datahub.io/core/country-list/r/0.csv | grep $cc | awk -F "," '{print $1}'
